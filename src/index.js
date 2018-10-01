38b82cd674f3feab3c7fbaca3c00c1ae570a7603
MaximusMiloi <Maximusmiloi@gmail.com>
2018-10-02 00:23:37 +0300

    Task 8. Advaced-zeros.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  function inc(obj, key) {
+    if (!obj[key]) {
+      obj[key] = 0;
+    }
+    obj[key]++;
+  }
+  
+  function getPrimeFactors(num) {
+    var primeFactors = {};
+    while (num % 2 === 0) {
+      inc(primeFactors, 2);
+      num = num / 2;
+    }
+  
+    var sqrtNum = Math.sqrt(num);
+    for (var i = 3; i <= sqrtNum; i++) {
+      while (num % i === 0) {
+        inc(primeFactors, i);
+        num = num / i;
+      }
+    }
+  
+    if (num > 2) {
+      inc(primeFactors, num);
+    }
+    return primeFactors;
+  }
+  const primes = getPrimeFactors(base);
+  let count = [];
+  Object.keys(primes).map(v => parseInt(v)).forEach(prime => {
+    let sum = 0;
+    let value = prime;
+    while (value <= number) {
+      sum += Math.floor(number / value);
+      value *= prime;
+    }
+    count.push(Math.floor(sum / primes[prime]));
+  });
+
+  return Math.min(...count);
 }
